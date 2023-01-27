242629e7236917026f3bf57581d2621595a4ff70
AllanCortes <91397441+AllanCortes@users.noreply.github.com>
2023-01-27 19:05:43 -0300

    Add: UI shop, little refactoring

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,20 +4,24 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule,
+    PaginationModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
